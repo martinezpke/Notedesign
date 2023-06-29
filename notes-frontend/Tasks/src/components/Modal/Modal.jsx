7197cbf1a24df2b109371589ace297b83abb43ce
Kevin Martínez Palmieri <martinezpke26@gmail.com>
2023-06-29 10:03:56 -0500

    aplicamos el metodo Debounce para dar mejor rendimiento

--- notes-frontend/Tasks/src/components/Modal/Modal.jsx
+++ notes-frontend/Tasks/src/components/Modal/Modal.jsx
@@ -1,43 +1,84 @@
-import React, {  useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useMemo } from 'react';
+import { updateNote } from '../../api/note.api';
 import './Modal.css'
 
-const Modal = ( { title, description, onClick, data} ) => {
+const Modal = ({ title, description, onClick, data, id, updateData }) => {
   const hightText = useRef(null);
   const [isSaveBtn, setIsSaveBtn] = useState(false);
   const [valueTitle, setValueTitle] = useState(title);
   const [valueDescription, setValueDescription] = useState(description);
 
+  // variable que compara si es verdad o falso para cambiar el logo
   const icon = isSaveBtn ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
     <path fillRule="evenodd" d="M6.32 2.577a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 01-1.085.67L12 18.089l-7.165 3.583A.75.75 0 013.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93z" clipRule="evenodd" />
   </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
   </svg>
-
+  
+  // funcion que cambia el estado de isSaveBtn
   const handleClick = () => {
     setIsSaveBtn(!isSaveBtn);
   }
+
 
   const handleAutoHight = () => {
     const textarea = hightText.current;
     textarea.style.height = 'auto'; // Reiniciar la altura a "auto" para que se recalcule correctamente
     textarea.style.height = `${textarea.scrollHeight}px`; // Ajustar la altura al valor de desplazamiento del contenido
   };
-  
+
   const handleChangeTitle = (e) => {
+    const newTitle = e.target.value;
+    console.log(newTitle);
+    setValueTitle(newTitle);
     setValueTitle(e.target.value);
   };
 
-  const handleChangeDescription = (e) => {
+  const handleChangeDescription =  (e) => {
     setValueDescription(e.target.value);
     handleAutoHight(e)
   }
 
+  const updateNotaTime = async (idNota, title, description) => {
+    const noteData = {
+      title: title,
+      description: description,
+    }
+
+    
+
+    try {
+      const response = await updateNote(idNota, noteData);
+      updateData()
+      console.log("Nota actulizada:", response.data);
+
+    } catch (error) {
+      console.error("Error al actulizar:", error);
+    }
+  }
+
+
   data(valueTitle, valueDescription)
+
+  const debouncedUpdateNote = useMemo(() => {
+    const debounceTimer = setTimeout(() => {
+      updateNotaTime(id, valueTitle, valueDescription);
+    }, 500); // el tiempo de espera
+
+    return () => {
+      clearTimeout(debounceTimer);
+    };
+  },[id, valueTitle, valueDescription]);
 
   useEffect(() => {
     handleAutoHight(); // Llamar a la función handleAutoHeight cuando cambie la descripción
-  }, [description]);
-  
+    debouncedUpdateNote();
+
+    return () => {
+      clearTimeout(debouncedUpdateNote);
+    };
+  }, [debouncedUpdateNote]);
+
 
   return (
     <>
@@ -47,7 +88,7 @@ const Modal = ( { title, description, onClick, data} ) => {
           <div className="container-text">
             <div className="modal-header">
               <div className='modal-header-container'>
-                <input type="text" value={valueTitle} onChange={handleChangeTitle} placeholder='Titulo'/>
+                <input type="text" value={valueTitle} onChange={handleChangeTitle} placeholder='Titulo' />
               </div>
 
               <button className='btn-save' onClick={handleClick}>
